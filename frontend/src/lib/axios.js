b65545d39af73b924ea019e26c8d4f1ab4b35900
Ravi <raviy3083@gmail.com>
2026-03-14 22:53:15 +0530

    redis disables

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001/api"
-    : "https://notes-backend.onrender.com/api";
+    : "https://note-g873.onrender.com";
 
 const api = axios.create({
   baseURL: BASE_URL,
